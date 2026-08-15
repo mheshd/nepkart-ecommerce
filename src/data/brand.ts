e7447d9a708533586289096d6377e1ab76f5af7a
mheshd <delluser946@gmail.com>
2026-08-15 23:04:49 +0545

    refactor: add brand data

---
+++ src/data/brand.ts
@@ -0,0 +1,37 @@
+export interface BrandDisplay {
+  slug: string;
+  label: string;
+  logo: string;
+}
+
+export const brandDisplays: BrandDisplay[] = [
+  { slug: "NOVA", label: "NOVA", logo: "/images/brands/nova.webp" },
+  {
+    slug: "StreetForm",
+    label: "StreetForm",
+    logo: "/images/brands/streetform.png",
+  },
+  {
+    slug: "TrailPeak",
+    label: "TrailPeak",
+    logo: "/images/brands/trailpeak.png",
+  },
+  { slug: "Forme", label: "Forme", logo: "/images/brands/forme.jpg" },
+  {
+    slug: "UrbanThread",
+    label: "UrbanThread",
+    logo: "/images/brands/urbanthread.png",
+  },
+  {
+    slug: "SoundCore",
+    label: "SoundCore",
+    logo: "/images/brands/soundcore.png",
+  },
+  { slug: "WorkForm", label: "WorkForm", logo: "/images/brands/workform.png" },
+  { slug: "KeyLab", label: "KeyLab", logo: "/images/brands/keylab.png" },
+  {
+    slug: "NOVA Home",
+    label: "NOVA Home",
+    logo: "/images/brands/nova-home.jpg",
+  },
+];
