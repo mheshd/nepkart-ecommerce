6b16bf4ad6983ccd02ed9935122b998dbf8c1bac
mheshd <delluser946@gmail.com>
2026-08-16 18:20:45 +0545

    feat: map category and display with link

--- src/features/category/CategoryList.tsx
+++ src/features/category/CategoryList.tsx
@@ -1,5 +1,19 @@
-const CategoryList = () => {
-  return <div></div>;
+import { Link } from "react-router-dom";
+
+interface CategoryListProps {
+  categories: string[];
+}
+
+const CategoryList = ({ categories }: CategoryListProps) => {
+  return (
+    <div>
+      {categories.map((category) => (
+        <Link key={category} to={`/category/${category}`}>
+          {category}
+        </Link>
+      ))}
+    </div>
+  );
 };
 
 export default CategoryList;
