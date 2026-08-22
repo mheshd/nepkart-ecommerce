4b33558d141e3cc78bcf6c8f95940ed9e6f154ec
mheshd <delluser946@gmail.com>
2026-08-22 21:13:33 +0545

    feat: render categoryGrid, barndGrid and productGrid

--- src/page/Homepage.tsx
+++ src/page/Homepage.tsx
@@ -1,5 +1,22 @@
+import BrandGrid from "../features/brand/BrandGrid";
+import CategoryGrid from "../features/category/CategoryGrid";
+import ProductGrid from "../features/product/components/ProductGrid";
+import { useProducts } from "../features/product/hooks/useProducts";
+import {
+  getBrandDisplay,
+  getCategoryDisplays,
+} from "../services/productService";
+
 const Homepage = () => {
-  return <div></div>;
+  const { loading, products } = useProducts();
+
+  return (
+    <div className=" w-full max-w-7xl mx-auto py-8">
+      <CategoryGrid categories={getCategoryDisplays()} />
+      <BrandGrid brands={getBrandDisplay()} />
+      <ProductGrid products={products} />
+    </div>
+  );
 };
 
 export default Homepage;
