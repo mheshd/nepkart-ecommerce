027649a96717cd30f9e037a8e86b1fe7d80e3d3c
mheshd <delluser946@gmail.com>
2026-08-22 13:46:57 +0545

    feat: render brand price size and rating filter

---
+++ src/features/filters/components/FilterSidebar.tsx
@@ -0,0 +1,51 @@
+// 	Composes all the individual filter inputs together
+// why
+// One container ProductListingPage renders, instead of 5 separate ones
+
+import BrandFilter from "./BrandFilter";
+import PriceRangeFilter from "./PriceRangeFilter";
+import RatingFilter from "./RatingFilter";
+import SizeFilter from "./SizeFilter";
+interface FilterSidebarProps {
+  brands: string[];
+  selectedBrands: string[];
+  onToggleBrand: (brand: string) => void;
+  sizes: string[];
+  selectedSizes: string[];
+  onToggleSize: (size: string) => void;
+  priceRange: [number, number];
+  onApply: (max: number, min: number) => void;
+  minRating: number;
+  setMinRating: (rating: number) => void;
+}
+const FilterSidebar = ({
+  brands,
+  selectedBrands,
+  onToggleBrand,
+  sizes,
+  selectedSizes,
+  onToggleSize,
+  priceRange,
+  onApply,
+  minRating,
+  setMinRating,
+}: FilterSidebarProps) => {
+  return (
+    <div className="w-40">
+      <BrandFilter
+        brandlist={brands}
+        selectedBrands={selectedBrands}
+        onToggle={onToggleBrand}
+      />
+      <SizeFilter
+        sizes={sizes}
+        selectedSizes={selectedSizes}
+        onToggle={onToggleSize}
+      />
+      <PriceRangeFilter priceRange={priceRange} onApply={onApply} />
+      <RatingFilter minRating={minRating} onChange={setMinRating} />
+    </div>
+  );
+};
+
+export default FilterSidebar;
