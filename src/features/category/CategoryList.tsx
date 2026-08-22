6a9dbfcbdbe004aca1855a04b2591522a8ba39db
mheshd <delluser946@gmail.com>
2026-08-22 13:43:43 +0545

    feat: make category list for navbar

--- src/features/category/CategoryList.tsx
+++ src/features/category/CategoryList.tsx
@@ -1,14 +1,15 @@
 import { Link } from "react-router-dom";
+import { getCategories } from "../../services/productService";
 
-interface CategoryListProps {
-  categories: string[];
-}
-
-const CategoryList = ({ categories }: CategoryListProps) => {
+const CategoryList = () => {
   return (
-    <div>
-      {categories.map((category) => (
-        <Link key={category} to={`/category/${category}`}>
+    <div className=" flex gap-2">
+      {getCategories().map((category) => (
+        <Link
+          key={category}
+          to={`/category/${category}`}
+          className=" flex  gap-2"
+        >
           {category}
         </Link>
       ))}
