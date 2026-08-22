b6b6f1bbe63f4a8fed8dd0f8ad663f349698b3df
mheshd <delluser946@gmail.com>
2026-08-22 11:37:13 +0545

    feat: make a resuable button

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,5 +1,29 @@
-const Button = () => {
-  return <div></div>;
+import type { ButtonHTMLAttributes } from "react";
+
+interface Btnprops extends ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: "primary" | "secondary";
+}
+
+const variants = {
+  primary: " bg-black text-white",
+  secondary: "bg-white text-black",
+};
+
+const Button = ({
+  children,
+  variant = "primary",
+  className = "",
+  ...props
+}: Btnprops) => {
+  return (
+    <button
+      {...props}
+      className={` rounded-md text-sm  cursor-pointer 
+        ${variants[variant]} ${className}`}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
