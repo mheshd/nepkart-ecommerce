6bd41137fbb1cb24684d0d38f3a539a7ccc85670
mheshd <delluser946@gmail.com>
2026-08-22 11:38:30 +0545

    feat: make resuable input

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,5 +1,27 @@
-const Input = () => {
-  return <div></div>;
+import type { InputHTMLAttributes } from "react";
+
+interface inputProps extends InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+  varient?: "primary";
+}
+const varients = {
+  primary: "border border-gray-300 px-4 py-3 ",
+};
+
+const Input = ({
+  label,
+  varient = "primary",
+  className = "",
+  ...props
+}: inputProps) => {
+  return (
+    <input
+      type="text"
+      className={`w-full rounded-lg bg-white text-sm text-gray-900 outline-none
+         ${varients[varient]} ${className}`}
+      {...props}
+    />
+  );
 };
 
 export default Input;
