991ebe5976e49ed8415870fe806f114e655fae60
mheshd <delluser946@gmail.com>
2026-08-22 11:45:24 +0545

    feat: create add to card button

--- src/features/Cart/components/AddToCartButton.tsx
+++ src/features/Cart/components/AddToCartButton.tsx
@@ -1,2 +1,47 @@
-// Receive the product as a prop, Read dispatch from CartContext, Validate before allowing the click, On click, dispatch an action to cartReducer
-// Local UI feedback state
+import Button from "../../../components/ui/Button";
+import type { Product } from "../../../types/productType";
+import { useCartContext } from "../context/CartContext";
+interface AddToCartButtonProps {
+  product: Product;
+  selectedSize: string | null;
+  selectedColor: string | null;
+  quantity: number;
+}
+const AddToCartButton = ({
+  product,
+  selectedSize,
+  selectedColor,
+  quantity,
+}: AddToCartButtonProps) => {
+  const { addToCart } = useCartContext();
+
+  const needsSize = product.sizes.length > 1 || product.sizes[0] !== "One Size";
+  const canAdd = !needsSize || selectedSize !== null;
+
+  function handleClick() {
+    if (!canAdd) return;
+    addToCart({
+      productId: product.id,
+      name: product.name,
+      price: product.price,
+      image: product.images[0],
+      size: selectedSize,
+      color: selectedColor,
+      quantity,
+    });
+  }
+
+  return (
+    <div>
+      <Button
+        onClick={handleClick}
+        disabled={!canAdd}
+        className=" px-6 py-2 font-medium  disabled:opacity-40"
+      >
+        Add to cart
+      </Button>
+    </div>
+  );
+};
+
+export default AddToCartButton;
