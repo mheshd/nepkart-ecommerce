0b8250fa935658720de15b75d3421930f7473d79
mheshd <delluser946@gmail.com>
2026-08-23 18:42:45 +0545

    refactor: change result to results

--- src/features/search/hooks/useSearch.ts
+++ src/features/search/hooks/useSearch.ts
@@ -2,24 +2,24 @@ import { useState, useEffect } from "react";
 import { searchProducts } from "../../../services/productService";
 import type { Product } from "../../../types/productType";
 
-export function UseSearch(query: string) {
-  const [result, setResult] = useState<Product[]>([]);
+export function useSearch(query: string) {
+  const [results, setResults] = useState<Product[]>([]);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     if (!query.trim()) {
-      setResult([]);
+      setResults([]);
       setLoading(false);
       return;
     }
     setLoading(true);
 
     const timer = setTimeout(() => {
-      setResult(searchProducts(query));
+      setResults(searchProducts(query));
       setLoading(false);
     }, 300);
     return () => clearTimeout(timer);
   }, [query]);
 
-  return { loading, result };
+  return { loading, results };
 }
