7be7a203691374491f5f034fe44539d8c9f914a8
mheshd <delluser946@gmail.com>
2026-08-23 18:41:49 +0545

    style: make responsive search suggestions

--- src/features/search/components/SearchSuggestions.tsx
+++ src/features/search/components/SearchSuggestions.tsx
@@ -11,17 +11,20 @@ const SearchSuggestions = ({
   onSelect,
   loading,
 }: SearchSuggestionsProps) => {
-  if (product.length === 0) return null;
+  if (product.length === 0 && !loading) return null;
 
   return (
-    <ul className=" absolute top-full left-0 w-full  bg-white shadow-md z-10">
+    <ul
+      className=" absolute top-full left-0 w-full  bg-gray-50 border-t border-gray-100
+     shadow-md z-10 p-2"
+    >
       {loading && <li className="px-3 py-2">Loading...</li>}
       {product.map((product) => (
         <li key={product.slug}>
           <button
             type="button"
             onClick={() => onSelect(product)}
-            className="block w-full text-left px-3 py-2 hover:bg-gray-50"
+            className="block w-full text-left px-3 py-2 hover:bg-gray-50 cursor-pointer"
           >
             {product.name}
           </button>
