4fa7046385ca4586ffe1d0f43f22d98b19a6d6d2
mheshd <delluser946@gmail.com>
2026-08-23 22:03:40 +0545

    style: create responsive categoryList and adjust imge size

--- src/features/category/CategoryGrid.tsx
+++ src/features/category/CategoryGrid.tsx
@@ -7,13 +7,22 @@ interface CategoryGridProps {
 
 const CategoryGrid = ({ categories }: CategoryGridProps) => {
   return (
-    <div>
-      <h2>Categories</h2>
-      <div className=" grid grid-cols-4 gap-2">
+    <div className="bg-white p-2">
+      <h2 className=" font-heading  text-xl text-gray-600 mb-2 ">Categories</h2>
+      <div className=" grid grid-cols-3 sm:grid-cols-4 md:grid-cols-6  md:gap-0 gap-1 ">
         {categories.map((category) => (
-          <Link to={`/category/${category.slug}`} key={category.slug}>
-            <img src={category.image} alt={category.label} />
-            <span>{category.label}</span>
+          <Link
+            to={`/category/${category.slug}`}
+            key={category.slug}
+            className=" flex flex-col items-center  gap-2 text-center  p-4 border
+             border-gray-100 transition-shadow duration-200 hover:shadow-2xl "
+          >
+            <img
+              src={category.image}
+              alt={category.label}
+              className=" w-full aspect-square object-cover  "
+            />
+            <span className=" font-body ">{category.label}</span>
           </Link>
         ))}
       </div>
