cfde18b839bf29b960d11284091f564a9561037f
mheshd <delluser946@gmail.com>
2026-08-23 18:45:10 +0545

    style: make responsive category list UI

--- src/features/category/CategoryList.tsx
+++ src/features/category/CategoryList.tsx
@@ -1,14 +1,20 @@
 import { Link } from "react-router-dom";
 import { getCategories } from "../../services/productService";
 
-const CategoryList = () => {
+interface CategoryListProps {
+  onSelect?: () => void;
+}
+
+const CategoryList = ({ onSelect }: CategoryListProps) => {
   return (
-    <div className=" flex gap-2">
+    <div className=" flex sm:flex-row flex-col items-center gap-2 sm:gap-8 sm:py-1 py-2  px-4 ">
       {getCategories().map((category) => (
         <Link
           key={category}
           to={`/category/${category}`}
-          className=" flex  gap-2"
+          onClick={onSelect}
+          className="flex sm:flex-col   items-center text-md  sm:text-sm text-gray-600
+           hover:text-black transition-colors group "
         >
           {category}
         </Link>
