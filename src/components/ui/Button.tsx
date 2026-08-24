c33c220969df3706b6f4d10529e5b67e20db3f84
mheshd <delluser946@gmail.com>
2026-08-24 20:57:01 +0545

    feat: add gitmerge to avoid costom color conflict

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,13 +1,13 @@
 import type { ButtonHTMLAttributes } from "react";
+import { twMerge } from "tailwind-merge";
 
 interface Btnprops extends ButtonHTMLAttributes<HTMLButtonElement> {
-  variant?: "primary" | "secondary" | "accent";
+  variant?: "primary" | "secondary";
 }
 
 const variants = {
   primary: " bg-black text-white",
   secondary: "bg-white text-black",
-  accent: "bg-[#F85606] text-white hover:bg-[#e04d04]",
 };
 
 const Button = ({
@@ -19,8 +19,11 @@ const Button = ({
   return (
     <button
       {...props}
-      className={` rounded-md text-sm  cursor-pointer 
-        ${variants[variant]} ${className}`}
+      className={twMerge(
+        "rounded-md text-sm cursor-pointer",
+        variants[variant],
+        className,
+      )}
     >
       {children}
     </button>
