efe6c7b508d7a814c87ea121841ee09f3f732c9b
mheshd <delluser946@gmail.com>
2026-08-24 20:58:28 +0545

    style: remove varient and add costom color

--- src/features/Cart/components/AddToCartButton.tsx
+++ src/features/Cart/components/AddToCartButton.tsx
@@ -34,10 +34,9 @@ const AddToCartButton = ({
   return (
     <div>
       <Button
-        variant="accent"
         onClick={handleClick}
         disabled={!canAdd}
-        className=" px-6 py-2 font-medium   disabled:opacity-40"
+        className=" px-6 py-2 font-medium bg-[#F85606] text-white hover:bg-[#e04d04]  disabled:opacity-40"
       >
         Add to cart
       </Button>
