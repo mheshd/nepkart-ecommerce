b39a38a35c9e9cb4a5cf3f4174e7523aaafc72b8
mheshd <delluser946@gmail.com>
2026-08-24 17:54:20 +0545

    style: change padding top on main

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -6,7 +6,7 @@ const AppLayout = ({ children }: { children: ReactNode }) => {
   return (
     <div className=" min-h-screen flex flex-col">
       <Navbar />
-      <main className=" flex-1  pt-24">{children}</main>
+      <main className=" flex-1  pt-27">{children}</main>
       <Footer />
     </div>
   );
