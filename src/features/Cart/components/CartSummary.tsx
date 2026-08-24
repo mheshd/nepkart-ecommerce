8a61c0ab93d717a734cdde22ef2a460e46f10a1e
mheshd <delluser946@gmail.com>
2026-08-24 16:29:59 +0545

    style: polish CartSummary  typography and CTA styling

--- src/features/Cart/components/CartSummary.tsx
+++ src/features/Cart/components/CartSummary.tsx
@@ -12,21 +12,30 @@ const CartSummary = ({ itemCount, subtotal }: CartSummaryProps) => {
   const total = subtotal + shippingFee;
 
   return (
-    <div className="border rounded-md p-4 h-fit">
-      <h2 className="font-semibold mb-3">Order Summary</h2>
+    <div className=" bg-white shadow-md  rounded-lg px-4 py-6 h-fit space-y-5">
+      <h2 className="font-semibold font-heading mb-3">Order Summary</h2>
+      <div className="border-t border-gray-200" />
 
-      <div className="flex items-center justify-between text-sm mb-2">
-        <span>Subtotal ({itemCount}) Items</span>
-        <span>{formatCurrency(subtotal)}</span>
+      <div className="flex items-center justify-between text-sm ">
+        <span className="font-body text-gray-700">
+          Subtotal ({itemCount}) Items
+        </span>
+        <span className="font-body text-[#F85606] text-base">
+          {formatCurrency(subtotal)}
+        </span>
       </div>
-      <div className="flex items-center justify-between text-sm mb-2">
-        <span>Shipping Fee</span>
-        <span>{formatCurrency(shippingFee)}</span>
+      <div className="flex items-center justify-between text-sm ">
+        <span className=" font-body text-gray-700">Shipping Fee</span>
+        <span className="font-body text-[#F85606] text-base">
+          {formatCurrency(shippingFee)}
+        </span>
       </div>
 
-      <div className="flex items-center justify-between font-medium border-t pt-2 mt-2">
-        <span>Total</span>
-        <span>{formatCurrency(total)}</span>
+      <div className="flex items-center justify-between font-medium border-t border-gray-200 pt-2 mt-2">
+        <span className="  font-body">Total</span>
+        <span className="font-body text-[#F85606] text-base">
+          {formatCurrency(total)}
+        </span>
       </div>
 
       <Link
@@ -35,7 +44,7 @@ const CartSummary = ({ itemCount, subtotal }: CartSummaryProps) => {
         className={`block text-center mt-4 px-4 py-2 rounded-md text-sm font-medium ${
           itemCount === 0
             ? "bg-gray-200 text-gray-400 pointer-events-none"
-            : "bg-black text-white"
+            : "bg-[#5FA1ED] text-white hover:bg-[#e04d04]"
         }`}
       >
         Proceed to Checkout
