4753b3b1e54d2718767922d8478ed32665014683
mheshd <delluser946@gmail.com>
2026-08-24 16:25:36 +0545

    style: change icon color

--- src/features/Cart/components/CartIcon.tsx
+++ src/features/Cart/components/CartIcon.tsx
@@ -13,10 +13,10 @@ const CartIcon = () => {
       className="relative"
       aria-label={`Cart, ${itemCount} item${itemCount !== 1 ? "s" : ""}`}
     >
-      <ShoppingCart size={20} aria-hidden="true" />
+      <ShoppingCart size={25} aria-hidden="true" className="text-gray-800" />
       {itemCount > 0 && (
         <span
-          className=" absolute -top-3 -right-2  bg-black text-white text-xs rounded-full w-4 h-4 flex items-center justify-center "
+          className=" absolute -top-3 -right-2  bg-gray-700 text-white text-xs rounded-full w-4 h-4 flex items-center justify-center "
           aria-hidden="true"
         >
           {itemCount}
