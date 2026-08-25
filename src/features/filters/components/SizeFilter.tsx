40527dbe1b21ce72f12f4a80df17594d329205c6
mheshd <delluser946@gmail.com>
2026-08-25 16:45:19 +0545

    style: change ui of text and size

--- src/features/filters/components/SizeFilter.tsx
+++ src/features/filters/components/SizeFilter.tsx
@@ -8,18 +8,26 @@ interface SizeFilterProps {
 
 const SizeFilter = ({ sizes, selectedSizes, onToggle }: SizeFilterProps) => {
   return (
-    <div>
-      <h2>Size</h2>
-      <ul>
-        {sizes.map((size) => (
-          <li key={size}>
-            <Checkbox
-              checked={selectedSizes.includes(size)}
-              onChange={() => onToggle(size)}
-              label={size}
-            />
-          </li>
-        ))}
+    <div className="mt-2">
+      <h2 className="font-heading text-sm font-semibold text-gray-800 uppercase tracking-wide mb-1 ">
+        Sizes
+      </h2>
+      <ul className=" flex flex-col gap-1">
+        {sizes.map((size) => {
+          const selected = selectedSizes.includes(size);
+          return (
+            <li key={size}>
+              <Checkbox
+                checked={selected}
+                onChange={() => onToggle(size)}
+                label={size}
+                className={
+                  selected ? "text-gray-900 font-medium" : "text-gray-600"
+                }
+              />
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
